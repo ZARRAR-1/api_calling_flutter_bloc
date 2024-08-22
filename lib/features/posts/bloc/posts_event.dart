d0ceb9a01c6168e3740248bcdc43bba1b636fe2c
ZARRAR-1 <mack4009@gmail.com>
2024-08-22 21:22:22 +0500

    Initial commit

---
+++ lib/features/posts/bloc/posts_event.dart
@@ -0,0 +1,8 @@
+part of 'posts_bloc.dart';
+
+@immutable
+sealed class PostsEvent {}
+
+class PostsFetchEvent extends PostsEvent {}
+
+class PostAddEvent extends PostsEvent {}
---
+++ lib/features/posts/bloc/posts_state.dart
@@ -0,0 +1,23 @@
+part of 'posts_bloc.dart';
+
+@immutable
+sealed class PostsState {} //Auto generated
+
+abstract class PostsActionState {}//self-made
+
+final class PostsInitial extends PostsState {} //Auto generated
+
+//Non-Action States
+class PostFetchingState extends PostsState {}
+
+class PostLoadingState extends PostsState {}
+
+class PostErrorState extends PostsState {}
+
+class PostFetchSuccessState extends PostsState {}
+
+//Action States
+class PostAddSuccessState extends PostsActionState {}
+
+class PostAddFailureState extends PostsActionState {}
+
